b98ad28b49a5a5c5a55e3cca2250cbb4f69573d5
MarcoBG <140689432+MarcoVLGE@users.noreply.github.com>
2025-01-15 19:56:41 +0100

    Update Coin.cs

--- Assets/Coin.cs
+++ Assets/Coin.cs
@@ -1,19 +1,20 @@
 using Fusion;
+using StarterAssets;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Coin : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-
+        if (other.GetComponent<ThirdPersonController>())
+        {
+            if (HasStateAuthority)
+            {
+                Runner.Despawn(GetComponent<NetworkObject>());
+            }
+        }
     }
 }
