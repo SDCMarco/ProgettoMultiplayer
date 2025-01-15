75a1e1572d5bcaaca47cbf7abba312a71020d271
MarcoBG <140689432+MarcoVLGE@users.noreply.github.com>
2025-01-15 20:09:28 +0100

    Update Coin.cs

--- Assets/Coin.cs
+++ Assets/Coin.cs
@@ -15,6 +15,11 @@ public class Coin : NetworkBehaviour
             {
                 Runner.Despawn(GetComponent<NetworkObject>());
             }
+            else
+            {
+                gameObject.SetActive(false);
+            }
+            
         }
     }
 }
